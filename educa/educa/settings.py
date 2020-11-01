d9887fe79facfed4ba3079d4b2e0d446869d8955
starkeyzachary <60682319+starkeyzachary@users.noreply.github.com>
2020-11-01 06:48:51 -0700

    educa

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=$z#p(r5okulew34y&aju1bw+&_#$)5xgyzh(5c7)6$0&qyay0'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
